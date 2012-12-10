48760f129c62c71071d146b4fc77caf8bfa09643
Daniel <robotright@gmail.com>
2012-12-10 15:57:05 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BibleRush2::Application.config.secret_token = '6adfec84a32997bd28aae6d5828af9a830e993b54ed22e024a51be1517c5b50451ee7a3a870d9cbcb0f13c35106522c8b98a41e3728c1b7ea92bf7b8e012b32a'
