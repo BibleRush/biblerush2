d65c113e780e307134b119f452e44358289108ea
Daniel <robotright@gmail.com>
2013-02-02 17:22:56 +0800

    logged in users should not see home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 # encoding: UTF-8
 
 class PagesController < ApplicationController
+  before_filter :check_logged_in
+
+  def check_logged_in
+    redirect_to dashboard_home_path if user_signed_in?
+  end
 end
