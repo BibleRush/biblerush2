4947cb8016d30f285ba9856c0030ab109516fcaf
Daniel <robotright@gmail.com>
2013-02-03 14:30:46 +0800

    add method to create Plans based on templates

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -8,4 +8,17 @@ class PlansController < ApplicationController
   def show
     @plan = Plan.find(params[:id])
   end
+
+  def new
+    template = PlanTemplate.find(params[:plan_template_id])
+    plan = Plan.create_from_template(template:   template,
+                                     start_date: DateTime.now.utc,
+                                     user:       current_user)
+    if plan.save!
+      redirect_to plan
+    else
+      flash[:error] = 'doh! something went wrong'
+      redirect_to dashboard_home_path
+    end
+  end
 end
--- app/models/plan.rb
+++ app/models/plan.rb
@@ -6,4 +6,18 @@ class Plan < ActiveRecord::Base
 
   validates :name, :presence => true
   validates :user_id, :presence => true
+
+  def self.create_from_template(args)
+    template   = args[:template]
+    start_date = args[:start_date]
+    user       = args[:user]
+
+    user.plans.create(name: "#{template.name} #{start_date}").tap do |plan|
+      template.plan_template_details.each do |ptd|
+        plan.plan_details.build(passage_ref: ptd.passage_ref, assigned_date: start_date)
+
+        start_date = start_date + 1.day
+      end
+    end
+  end
 end
