1ef3b5c9aa5e727cb7d84e6341ea785afd70be1b
Daniel <robotright@gmail.com>
2013-02-03 14:29:22 +0800

    show newer plans at top

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,6 +3,6 @@
 class DashboardController < ApplicationController
   def home
     @plan_templates = PlanTemplate.all
-    @plans = current_user.plans
+    @plans = current_user.plans.order('created_at DESC')
   end
 end
