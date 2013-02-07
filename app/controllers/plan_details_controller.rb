5a54492cc4f09254d1317c51b4c513490a551392
Daniel <robotright@gmail.com>
2013-02-07 23:56:08 +0800

    remove n+1 query

--- app/controllers/plan_details_controller.rb
+++ app/controllers/plan_details_controller.rb
@@ -1,7 +1,7 @@
 class PlanDetailsController < ApplicationController
   def show
     @plan_detail = PlanDetail.find(params[:id])
-    @comments = @plan_detail.comments
+    @comments = @plan_detail.comments.includes(:user)
     @new_comment = @plan_detail.comments.new
   end
 end
