2ca4bc2d312492b8d3f3f7b1fd453122f711476b
Daniel <robotright@gmail.com>
2013-02-11 09:26:25 +0800

    destroy associated comments if accounts ever destroyed

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,10 @@ class User < ActiveRecord::Base
   attr_accessible :username, :email, :password, :password_confirmation, :remember_me, :avatar
 
   before_create :assign_random_avatar
+  before_destroy :destroy_comments
+
   has_many :plans, :dependent => :destroy
+
   validates :avatar,
     inclusion: { in: EmojiHelper::EMOJI_NAMES.map { |a| ":#{a}:"},
       message: "emoji %{value} does not appear to exist" }, :on => :update
@@ -22,5 +25,13 @@ class User < ActiveRecord::Base
   # only for new users
   def assign_random_avatar
     self.avatar = ":#{EmojiHelper::EMOJI_NAMES.sample}:"
+  end
+
+  # using comment plugin so need to delete comments manually
+  #
+  # cannot delete based on plan_detail association since those are tied to
+  # plans, and only owned plans are associated with users
+  def destroy_comments
+    Comment.where(user_id: id).destroy_all
   end
 end
