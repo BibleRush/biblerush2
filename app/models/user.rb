a281d444d2088ead54b616978e7220128f7b76e2
Daniel <robotright@gmail.com>
2013-02-11 09:25:18 +0800

    add username to accounts

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,14 +6,16 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :avatar
-  # attr_accessible :title, :body
+  attr_accessible :username, :email, :password, :password_confirmation, :remember_me, :avatar
 
   before_create :assign_random_avatar
   has_many :plans, :dependent => :destroy
   validates :avatar,
     inclusion: { in: EmojiHelper::EMOJI_NAMES.map { |a| ":#{a}:"},
       message: "emoji %{value} does not appear to exist" }, :on => :update
+  # min is small since chinese names are also expected
+  validates :username, :length => { :in => 1..30 }
+  validates :username, :exclusion => { :in => %w[god], :message => 'is not allowed' }
 
   private
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130210154804) do
+ActiveRecord::Schema.define(:version => 20130211003005) do
 
   create_table "bible_infos", :force => true do |t|
     t.string   "version"
@@ -114,6 +114,7 @@ ActiveRecord::Schema.define(:version => 20130210154804) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.string   "avatar"
+    t.string   "username"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
