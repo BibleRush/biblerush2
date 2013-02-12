e0d1f8ef109b82140cf17899d828d8c4b0269b38
Daniel <robotright@gmail.com>
2013-02-12 17:18:07 +0800

    link to bible passage opens up parallel reading for Eng and Chinese

--- lib/utils.rb
+++ lib/utils.rb
@@ -1,7 +1,7 @@
 # encoding: UTF-8
 
 class Utils
-  def self.bgateway_link(passage, version='NIV')
+  def self.bgateway_link(passage, version='NIV;CUVMPS')
     "http://www.biblegateway.com/passage/?search=#{passage}&version=#{version}"
   end
 end
