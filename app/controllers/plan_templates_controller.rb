799311d99e3fbd970e76f61317107a5c0e1b9f74
Daniel <robotright@gmail.com>
2013-06-23 21:19:04 +0800

    change dashboard and flow for "start reading plan"

--- app/controllers/plan_templates_controller.rb
+++ app/controllers/plan_templates_controller.rb
@@ -1,4 +1,8 @@
 class PlanTemplatesController < ApplicationController
+  def index
+    @plan_templates = PlanTemplate.all
+  end
+
   def show
     @plan_template = PlanTemplate.find(params[:id])
     @plan_template_details = @plan_template.plan_template_details
