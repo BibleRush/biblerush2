c0713cee444add6b1d206a29b71af392ef1a42d9
Daniel <robotright@gmail.com>
2013-06-24 16:29:00 +0800

    keep flash message in case of double redirects

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,7 @@ class PagesController < ApplicationController
   before_filter :check_logged_in, :only => :landing
 
   def check_logged_in
+    flash.keep
     redirect_to dashboard_home_path if user_signed_in?
   end
 
