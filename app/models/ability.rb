a2e5f61bb5af358d05ffdb78a7711fa2bb3fa4fd
Daniel <robotright@gmail.com>
2013-12-17 22:51:34 +0800

    add special users that can view other people's plans

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,12 +7,18 @@ class Ability
     if signed_in?(user)
       can :manage, Plan, :user_id => user.id
       can :read, Plan do |plan|
-        user.plans.include?(plan)
+        user.plans.include?(plan) ||
+        special_users.include?(user.email)
       end
     end
   end
 
   def signed_in?(user)
     user.id.present?
+  end
+
+  def special_users
+    %w[daniel.kiros@gmail.com aaa@aaa.com sungchung@gmail.com chloeqywu@gmail.com
+       reolin0511@gmail.com]
   end
 end
