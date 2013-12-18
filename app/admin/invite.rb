6a558234f7098c2591141413002bd800d3829c58
Daniel <robotright@gmail.com>
2013-12-18 21:40:05 +0800

    customize invite model in activeadmin

--- app/admin/invite.rb
+++ app/admin/invite.rb
@@ -1,3 +1,13 @@
 ActiveAdmin.register Invite do
-
+  index do
+    column :id
+    column :email
+    column :avatar do |user|
+      EmojiHelper.emojatar(user.avatar)
+    end
+    column :invited_by
+    column :accepted
+    column :created_at
+    column :updated_at
+  end
 end
