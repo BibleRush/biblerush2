1b35105ab10bcf76caf6e7018f0afa32068416cc
Daniel <robotright@gmail.com>
2013-12-23 13:43:36 +0800

    add opt_in option for notification emails

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131206105716) do
+ActiveRecord::Schema.define(:version => 20131223053523) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "namespace"
@@ -137,8 +137,8 @@ ActiveRecord::Schema.define(:version => 20131206105716) do
   add_index "plans", ["user_id"], :name => "index_plans_on_user_id"
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "email",                  :default => "",   :null => false
+    t.string   "encrypted_password",     :default => "",   :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -147,10 +147,11 @@ ActiveRecord::Schema.define(:version => 20131206105716) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at",                               :null => false
+    t.datetime "updated_at",                               :null => false
     t.string   "avatar"
     t.string   "username"
+    t.boolean  "opt_in",                 :default => true
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
