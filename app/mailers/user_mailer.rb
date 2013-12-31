6cb132440c95528111b43612e4d2c59b9b4c9c43
Daniel <robotright@gmail.com>
2013-12-31 20:39:13 +0800

    include info on who invited you in email invite

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,11 @@
 class UserMailer < ActionMailer::Base
+  BIBLERUSH_URL = 'http://biblerush.com'
+
   default from: 'noreply@biblerush.com'
 
   def invite_to_plan_email(invite)
     @invite = invite
-    @url = 'http://biblerush.com'
+    @url = BIBLERUSH_URL
     mail(to: @invite.email, subject: "You're invited to BibleRush.com!")
   end
 end
