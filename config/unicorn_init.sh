c35bd9339f3c65d0808299530e22e64f73a986ae
Daniel <robotright@gmail.com>
2014-01-01 20:47:10 +0800

    fix unicorn restart

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -50,9 +50,9 @@ force-stop)
   echo >&2 "Not running"
   ;;
 restart|reload)
-  sig HUP && echo reloaded OK && exit 0
+  sig USR2 && sleep 5 && oldsig QUIT && echo "Killing old master" `cat $OLD_PIN` && exit 0
   echo >&2 "Couldn't reload, starting '$CMD' instead"
-  run "$CMD"
+  run $CMD
   ;;
 upgrade)
   if sig USR2 && sleep 2 && sig 0 && oldsig QUIT
