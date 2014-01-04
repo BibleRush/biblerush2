2e190e9d3a98c4ce1dd7356656859f1a2cb5f37e
Daniel <robotright@gmail.com>
2014-01-04 16:10:19 +0800

    add validations to PlanMembership

--- app/models/plan_membership.rb
+++ app/models/plan_membership.rb
@@ -3,4 +3,8 @@ class PlanMembership < ActiveRecord::Base
 
   belongs_to :user
   belongs_to :plan
+
+  validates :user_id, :uniqueness => { :scope => :plan_id }
+  validates :user_id, :presence => true
+  validates :plan_id, :presence => true
 end
