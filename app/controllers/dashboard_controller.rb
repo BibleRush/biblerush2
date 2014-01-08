045283a41c748ab364956fed10605cc7c6ab70b8
Daniel <robotright@gmail.com>
2014-01-08 20:11:43 +0800

    redirect if user is not logged in

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,10 +1,18 @@
 # encoding: UTF-8
 
 class DashboardController < ApplicationController
+  before_filter :require_login
+
   def home
     @plans = current_user.plans
     @finished_plans = nil
     #@plans = Plan.current_plans_for(current_user)
     #@finished_plans = Plan.finished_plans_for(current_user)
+  end
+
+  protected
+
+  def require_login
+    redirect_to root_path unless current_user
   end
 end
