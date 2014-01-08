314bc2bc88c6ac6ee8a6553b0b9e7add6ed69241
Daniel <robotright@gmail.com>
2014-01-08 20:23:11 +0800

    !@#$

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,10 +4,10 @@ class DashboardController < ApplicationController
   before_filter :require_login
 
   def home
-    #@plans = current_user.plans
-    #@finished_plans = nil
-    @plans = Plan.current_plans_for(current_user)
-    @finished_plans = Plan.finished_plans_for(current_user)
+    @plans = current_user.plans
+    @finished_plans = nil
+    #@plans = Plan.current_plans_for(current_user)
+    #@finished_plans = Plan.finished_plans_for(current_user)
   end
 
   protected
