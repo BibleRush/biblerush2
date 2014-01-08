108805b7644af91bfad8ad49b3cbcb82c8c5be55
Daniel <robotright@gmail.com>
2014-01-08 19:50:57 +0800

    pg fixed now that pg version has been upgraded?

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,9 +2,7 @@
 
 class DashboardController < ApplicationController
   def home
-    @plans = current_user.plans
-    @finished_plans = nil
-    #@plans = Plan.current_plans_for(current_user)
-    #@finished_plans = Plan.finished_plans_for(current_user)
+    @plans = Plan.current_plans_for(current_user)
+    @finished_plans = Plan.finished_plans_for(current_user)
   end
 end
