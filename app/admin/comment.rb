627e50820dae1af868c85073d725ddf4b315a5ba
Daniel <robotright@gmail.com>
2014-01-11 23:12:46 +0800

    more comment details on admin page

--- app/admin/comment.rb
+++ app/admin/comment.rb
@@ -1,3 +1,12 @@
 ActiveAdmin.register Comment, :as => 'PlanComment' do
-
+  index do
+    column :id
+    column 'Plan ID', :commentable_id
+    column :comment
+    column :user do |comment|
+      user = User.find(comment.user_id)
+      "#{user.username} -  #{user.email}"
+    end
+    column :created_at
+  end
 end
