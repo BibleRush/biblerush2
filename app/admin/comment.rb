82caf6046b69cb56a30923dfe90cbdee4fddcfed
Daniel <robotright@gmail.com>
2014-01-11 23:23:34 +0800

    get correct plan id (not plan detail id)

--- app/admin/comment.rb
+++ app/admin/comment.rb
@@ -1,7 +1,10 @@
 ActiveAdmin.register Comment, :as => 'PlanComment' do
   index do
     column :id
-    column 'Plan ID', :commentable_id
+    column 'Plan ID', :commentable_id do |comment|
+      pd = PlanDetail.find(comment.commentable_id)
+      pd.plan.id
+    end
     column :comment
     column :user do |comment|
       user = User.find(comment.user_id)
